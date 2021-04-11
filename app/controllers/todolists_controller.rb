5d83e267898a415868efd3fe4d970f41f4be7f2d
sasahria0909 <ec2-user@ip-172-31-46-213.ec2.internal>
2021-04-11 07:34:21 +0000

    [Finish] 3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,30 +1,29 @@
 class TodolistsController < ApplicationController
   def new
-    # viewへ渡すためのインスタンス変数にからのモデルオブジェクトを生成する
     @list = List.new
   end
-  
   def create
-    # データを新規登録するインスタンス制作
     list = List.new(list_params)
-    # データベースへの保存のためのsaveメソッド実行
     list.save
-    #トップ画面へリダイレクト
-     redirect_to todolist_path(list.id)
+    redirect_to todolist_path(list.id)
+  end
+  def index
+    @lists = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
-def index
-  @lists = List.all
-end
-
-def show
-  @list = List.find(params[:id])
-end
-
-private
-# ストロングパラメータ？？
-def list_params
-  params.require(:list).permit(:title, :body)
-end
-
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
-  get 'todolist/:id' =>'todolists#show', as:'todolist'
+  get 'todolists/:id' =>'todolists#show', as:'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit' , as:'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as:'update_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
