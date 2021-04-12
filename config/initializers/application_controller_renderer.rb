e67997a9225f115c16f8f054e354223b528c2b89
sasahria0909 <ec2-user@ip-172-31-46-213.ec2.internal>
2021-04-12 00:05:09 +0000

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '55718ee7c8e2c1081087e43684e5b22f566e1d33e516d0eec366e9d9b887c72d491d585166d52321bb1310e06e4319d9fa071f4b6cea3e6639b3aec63bf090ce'
